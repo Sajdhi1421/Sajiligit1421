2015923a49e1b2f60e108b1d941f36eef8bd0b25
Sajdhi1421 <123482346+Sajdhi1421@users.noreply.github.com>
2023-02-13 18:26:10 +0530

    Initial commit

---
+++ ass2.que3.c
@@ -0,0 +1,22 @@
+/* WAP to convert a small letter into capital letter and vice versa */
+
+#include <stdio.h>  
+
+int main ()  
+{  
+    char small,large;
+    int ascii;  
+      
+    printf (" Enter the Large alphabet: ");  
+    scanf (" %c", &large);  
+    ascii = large + 32;  
+    printf (" \n small alphabet is: %c", large, ascii);  
+      
+
+    printf ("\n\n Enter the Small alphabet: ");  
+    scanf (" %c", &small);  
+    ascii = small - 32;  
+    printf ("\n %c character in large alphabet is: %c", small, ascii);  
+    
+}
+
